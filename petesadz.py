46e5d3b79243c62171b723c943ebd824465b07d4
petesalcedo <peter.salcedo.diaz@gmail.com>
2021-12-24 17:23:21 -0500

    adding handlers

--- petesadz.py
+++ petesadz.py
@@ -1,3 +1,4 @@
+from logging import Logger
 import os 
 import sys
 import six
@@ -37,4 +38,36 @@ logging.config.dictConfig({
     }
 })
 
+
+NIKE_HOME_URL = "https://www.nike.com/us/en_us/"
+NIKE_CHECKOUT_URL = "https://www.nike.com/checkout"
+NIKE_CART_API_URL = "https://secure-store.nike.com/us/services/jcartService"
+
+LOGGER = logging.gerLogger()
+
+def run(driver, username, password, product_id, sku_id, shoe_size, login_time=None, release_time=None,
+        page_load_timeout=None, screenshot_path=None, purchase=False, num_retries=None):
+    driver.maximize_window()
+    driver.set_page_load_timeout(page_load_timeout)
+
+    if login_time:
+        LOGGER.info("Waiting until login time: " + login_time)
+        pause.until(date_parser.parse(login_time))
+
+    try:
+        login(driver=driver, username=username, password=password)
+    except Exception as e:
+        LOGGER.exception('failed to log in:' + str(e))
+        six.reraise(Exception, e, sys.exc_info()[2])
+
+    if release_time:
+        LOGGER.info("Waitting until release time: " + release_time)
+        pause.unil(date_parser.parse(release_time))
+
+    num_retries_attempted = 0
+    while True:
+        try:
+            try:
+                LOGGER.info("Adding item to cart")
+                add_item_to_cart
 
